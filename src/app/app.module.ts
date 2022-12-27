f29bdafc1a3a4c1fa3ace3d727dd27301d78d3d0
Edilberto Rios Garcia <dredysteam@gmail.com>
2022-12-27 11:09:30 -0500

    Header created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
+// container
 import { AppComponent } from './app.component';
+
+// components
+import { HeaderComponent } from './components/header/header.component';
 
 //feature modules
 import { AuthModule } from 'src/auth/auth.module';
@@ -11,8 +15,9 @@ import { AuthModule } from 'src/auth/auth.module';
 export const ROUTES: Routes = [];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, RouterModule.forRoot(ROUTES), AuthModule],
+  exports: [HeaderComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
