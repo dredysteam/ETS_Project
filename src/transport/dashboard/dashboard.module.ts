f8fa979bfde788484333c233e53bfe74d93b39b9
Edilberto Rios Garcia <dredysteam@gmail.com>
2022-12-31 02:26:42 -0500

    Hero component created

--- src/transport/dashboard/dashboard.module.ts
+++ src/transport/dashboard/dashboard.module.ts
@@ -5,10 +5,13 @@ import { Routes, RouterModule } from '@angular/router';
 //containers
 import { DashboardComponent } from './containers/dashboard/dashboard.component';
 
+// components
+import { HeroComponent } from './components/hero/hero.component';
+
 //Routes
 export const ROUTES: Routes = [{ path: '', component: DashboardComponent }];
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, HeroComponent],
   imports: [CommonModule, RouterModule.forChild(ROUTES)],
 })
 export class DashboardModule {}
